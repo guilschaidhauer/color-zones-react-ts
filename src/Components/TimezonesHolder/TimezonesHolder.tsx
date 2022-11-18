2306b15a318973edd3d2e607c2d6cdd972f1c579
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-11-17 23:23:11 -0300

    Add TimezonesHolder

---
+++ src/Components/TimezonesHolder/TimezonesHolder.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class TimezonesHolder extends React.Component {
+
+  render() {
+    const timezones = [
+      'Asia/Tokyo',
+      'Europe/Rome'
+    ]; 
+
+    return (
+      <div className="TimezonesHolder">
+      </div>
+    );
+  }
+}
+
+export default TimezonesHolder;
