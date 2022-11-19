b61db1284571f93e617fe69d5a8c764c35709f3d
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-11-18 21:12:49 -0300

    Add Minutes

---
+++ src/Components/Minutes/Minutes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Minutes extends React.Component {
+  render() {
+    return (
+      <div className="Minutes">
+      </div>
+    );
+  }
+}
+
+export default Minutes;
