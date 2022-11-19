4ac54bccc9169813129089a5ed9c9e3d33348bfe
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-11-18 21:14:56 -0300

    Add TimezoneDate

---
+++ src/Components/TimezoneDate/TimezoneDate.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TimezoneDate extends React.Component {
+
+  render() {
+    return (
+      <div className="TimezoneDate">
+      </div>
+    );
+  }
+}
+
+export default TimezoneDate;
