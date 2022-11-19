c5ba6ca69eb0a0673b047bbd3c084fe4f42da6df
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-11-18 21:10:33 -0300

    Add Colon

---
+++ src/Components/Colon/Colon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Colon extends React.Component {
+  render() {
+    return (
+      <div className="Colon">
+      </div>
+    );
+  }
+}
+
+export default Colon;
