905a56eba53775933739c4abd43e49f85f94c870
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-11-19 21:00:26 -0300

    Add Sao Paulo timezone

--- src/Components/TimezonesHolder/TimezonesHolder.tsx
+++ src/Components/TimezonesHolder/TimezonesHolder.tsx
@@ -4,12 +4,13 @@ import TimezoneStrip from '../TimezoneStrip/TimezoneStrip';
 class TimezonesHolder extends React.Component {
 
 	render() {
-		const timezoneNames: string[] = ['Asia/Tokyo', 'Europe/Rome'];
+		const timezoneNames: string[] = ['Asia/Tokyo', 'Europe/Rome', 'America/Sao_Paulo'];
 
 		return (
 			<div className="TimezonesHolder">
 				<TimezoneStrip timezoneName={timezoneNames[0]} />
 				<TimezoneStrip timezoneName={timezoneNames[1]} />
+				<TimezoneStrip timezoneName={timezoneNames[2]} />
 			</div>
 		);
 	}
