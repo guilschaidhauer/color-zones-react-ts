8d3a6d04c2d0643f76c4da5a634aebea947825d7
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-11-21 22:42:02 -0300

    Add NewTimezoneButton

---
+++ src/Components/NewTimezoneButton/NewTimezoneButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NewTimezoneButton.css';
+
+class NewTimezoneButton extends React.Component {
+  render() { 
+    return (  
+      <button className="newTimezoneButton">+</button>
+    ); 
+  }
+}
+ 
+export default NewTimezoneButton;
