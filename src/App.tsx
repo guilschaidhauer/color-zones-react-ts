42dcee4374f213cad8fe0f9e18fb74104d56677e
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-11-21 22:45:11 -0300

    Add NewTimezoneButton to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import NewTimezoneButton from './Components/NewTimezoneButton/NewTimezoneButton';
 import TimezonesHolder from './Components/TimezonesHolder/TimezonesHolder';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <TimezonesHolder />
+        <NewTimezoneButton />
       </header>
     </div>
   );
