a0d8e737fb650d48588228e36530ff937d0a931f
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-11-21 23:50:09 -0300

    Add timzoneList constant

---
+++ src/Constants/TimezoneList.tsx
@@ -0,0 +1,35 @@
+
+export const timezoneList = new Map([
+  ['Australia/Melbourne', 'Melbourne'],
+  ['Australia/Sydney', 'Sydney'],
+
+  ['Asia/Tokyo', 'Tokyo'],
+  ['Asia/Seoul', 'Seoul'],
+
+  ['Asia/Taipei', 'Taipei'],
+  ['Asia/Shanghai', 'Beijing'],
+  ['Asia/Hong_Kong', 'Hong Kong'],
+
+  ['Asia/Kolkata', 'Bangalore'],
+
+  ['Europe/Paris', 'Paris'],
+  ['Europe/Berlin', 'Walldorf'],
+  ['Europe/Budapest', 'Budapest'],
+  ['Europe/Madrid', 'Madrid'],
+  ['Europe/Zurich', 'Zurich'],
+  ['Europe/Rome', 'Rome'],
+
+  ['Europe/London', 'London'],
+  ['Europe/Dublin', 'Dublin'],
+
+  ['America/Sao_Paulo', 'São Leopoldo'],
+  ['America/Argentina/Buenos_Aires', 'Buenos Aires'],
+
+  ['America/Manaus', 'Manaus'],
+
+  ['America/Vancouver', 'Vancouver'],
+  ['America/Toronto', 'Toronto'],
+  ['America/New_York', 'New York'],
+
+  ['America/Los_Angeles', 'Los Angeles']
+]);
