944087e8dcda5b4969c99f105d6efdb6c76b1e87
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-11-22 22:15:51 -0300

    Add SelectBox

---
+++ src/Components/SelectBox/SelectBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SelectBox extends React.Component {
+  render() {
+    return (
+      <div className="SelectBox">
+        <label>Choose a timezone</label>
+      </div>
+    );
+  }
+}
+
+export default SelectBox;
