834602845283b3ebd8927b4131ef0fe914c62ad0
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-11-22 23:04:38 -0300

    Add handleClick on SelectBox

--- src/Components/NewTimezoneButton/NewTimezoneButton.tsx
+++ src/Components/NewTimezoneButton/NewTimezoneButton.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './NewTimezoneButton.css';
 
 class NewTimezoneButton extends React.Component {
-  render() { 
-    return (  
-      <button className="NewTimezoneButton">+</button>
-    ); 
+  handleClick(): void {
+    console.log("Hello");
+  }
+
+  render() {
+    return (
+      <button className="NewTimezoneButton" onClick={this.handleClick}>+</button>
+    );
   }
 }
- 
+
 export default NewTimezoneButton;
