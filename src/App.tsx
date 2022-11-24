3c7fab3465a561a3dd06f628e38933f143e48c57
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-11-23 21:51:19 -0300

    Add activeTimezonesList to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
+import React from 'react';
 import './App.css';
 import TimezonesHolder from './Components/TimezonesHolder/TimezonesHolder';
 import NewTimezoneForm from './Components/NewTimezoneForm/NewTimezoneForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <TimezonesHolder />
-        <NewTimezoneForm />
-      </header>
-    </div>
-  );
+type Props = {
+}
+
+type State = {
+  activeTimezoneNames: string[];
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      activeTimezoneNames: []
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <TimezonesHolder activeTimezoneNames={this.state.activeTimezoneNames}/>
+          <NewTimezoneForm />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/TimezonesHolder/TimezonesHolder.tsx
+++ src/Components/TimezonesHolder/TimezonesHolder.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import './TimezonesHolder.css';
 import TimezoneStrip from '../TimezoneStrip/TimezoneStrip';
 
-class TimezonesHolder extends React.Component {
+type Props = {
+	activeTimezoneNames: string[];
+}
+
+class TimezonesHolder extends React.Component<Props> {
+	constructor(props: Props) {
+    super(props);
+  }
 
 	render() {
-		const timezoneNames: string[] = ['Asia/Tokyo', 'Europe/Rome', 'America/Sao_Paulo'];
+		//const timezoneNames: string[] = ['Asia/Tokyo', 'Europe/Rome', 'America/Sao_Paulo'];
 
 		return (
 			<div className="TimezonesHolder">
-				{timezoneNames.map((timezoneName) =>
-					<TimezoneStrip key={timezoneName} timezoneName={timezoneName} width={100 / timezoneNames.length} />)
+				{this.props.activeTimezoneNames.map((timezoneName) =>
+					<TimezoneStrip key={timezoneName} timezoneName={timezoneName} width={100 / this.props.activeTimezoneNames.length} />)
 				}
 			</div>
 		);
