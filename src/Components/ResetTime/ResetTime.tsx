a7e02e3a5072ef58dedc93593a9cc1a243851574
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-11-28 22:38:06 -0300

    Add ResetTime

---
+++ src/Components/ResetTime/ResetTime.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class ResetTime extends React.Component {
+  render() {
+    return (
+      <button className="ResetTime">Reset time</button>
+    );
+  }
+}
+
+export default ResetTime;
