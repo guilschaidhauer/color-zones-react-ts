bf1c47553818869c071436a0732a37d958a1d0e5
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-12-07 21:17:27 -0300

    Add Colon animation

--- src/Components/Colon/Colon.tsx
+++ src/Components/Colon/Colon.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import './Colon.css'
 
-class Colon extends React.Component {
+type Props = {
+}
+
+type State = {
+  isVisible: boolean
+}
+
+class Colon extends React.Component<Props, State> {
+  private timerID: any;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isVisible: true
+    }
+  }
+
+  componentDidMount(): void {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount(): void {
+    clearInterval(this.timerID);
+  }
+
+  tick(): void {
+    this.setState({
+      isVisible: !this.state.isVisible
+    });
+  }
+
   render() {
     return (
-      <div className="Colon">:</div>
+      <div className={((this.state.isVisible) ? 'Colon' : 'ColonOff')}>:</div>
     );
   }
 }
